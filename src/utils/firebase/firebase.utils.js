d3f21c6921465f859a83ca362b70da169d505b51
Ajdin Komić <ajdin.komic12@gmail.com>
2022-07-15 17:23:12 +0200

    feat(authentication): Add function for creating user in db

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 import { firebaseConfig } from '../../env.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,5 +15,36 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
+// Firebase authentication config
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// Firestore db config
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // getting the user document reference
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  // getting the user data
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user doesn't exist in db, create new one
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('Error creating the user', error.message);
+    }
+  }
+
+  // return user document reference
+  return userDocRef;
+};
