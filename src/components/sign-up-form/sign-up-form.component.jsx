2b27895dfc7868fda467d0754e068c88c22fd52b
Ajdin Komić <ajdin.komic12@gmail.com>
2022-07-18 07:34:45 +0200

    feat(authentication): Create SignUp, FormInput and Button components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -19,7 +21,6 @@ const SignUpForm = () => {
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormFields({ ...formFields, [name]: value });
   };
 
@@ -33,9 +34,7 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
-
       await createUserDocumentFromAuth(user, { displayName });
-
       resetFormFields();
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
@@ -47,19 +46,16 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
 
       <form onSubmit={handleSubmit}>
         <FormInput label='Display Name' name='displayName' type='text' required onChange={handleChange} value={displayName} />
-
         <FormInput label='Email' name='email' type='email' required onChange={handleChange} value={email} />
-
         <FormInput label='Password' name='password' type='password' required minLength='6' onChange={handleChange} value={password} />
-
         <FormInput label='Confirm Password' name='confirmPassword' type='password' required minLength='6' onChange={handleChange} value={confirmPassword} />
-
-        <button type='submit'>Sign Up</button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </div>
   );
